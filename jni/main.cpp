29ab4c51b73d5b276c3963d57c6f32ac65f0c652
16Byte <pixeldevs.diego@gmail.com>
2015-08-17 14:13:09 -0600

    Added Common::getGameVersionString Hook

--- jni/main.cpp
+++ jni/main.cpp
@@ -10,6 +10,13 @@
 #define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__))
 
 JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
-	
+
+	MSHookFunction((void*)&Common::getGameVersionString, (void*)&Common_getGameVersionString_hook, (void**)&Common_getGameVersionString_real);
+
+	static std::string(*Common_getGameVersionString_real)();
+	static std::string Common_getGameVersionString_hook() {
+		return "MoarMCPE By 16Byte";
+	}
+
 	return JNI_VERSION_1_2;
 }
